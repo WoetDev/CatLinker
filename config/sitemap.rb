9e0d7202e07f88391cf3a799a99f81df349d12f1
SprouterB <wouter.bruynsteen@gmail.com>
2020-05-01 00:36:34 +0200

    Added index and locales to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,7 +2,7 @@
 require 'fog/aws'
 
 SitemapGenerator::Sitemap.default_host = "http://www.catlinker.com"
-
+SitemapGenerator::Sitemap.create_index = true
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: ENV['FOG_PROVIDER'],
                                                                     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                                                                     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
@@ -16,7 +16,7 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
 SitemapGenerator::Sitemap.sitemaps_host = "http://s3-#{ENV['FOG_REGION']}.amazonaws.com/#{ENV['FOG_DIRECTORY']}/"
 # pick a namespace within your bucket to organize your maps
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-
+# ping Google & Bing from redirect route
 SitemapGenerator::Sitemap.ping_search_engines('http://www.catlinker.com/sitemap')
 
 SitemapGenerator::Sitemap.create do
@@ -26,45 +26,22 @@ SitemapGenerator::Sitemap.create do
 
       add cats_path, :changefreq => 'always'
       Cat.joins(:user).where(users: { is_cattery: true }).is_parent(false).is_available(true).find_each do |cat|
-        add cat_path(cat), :priority => '1.0', :changefreq => 'always', :lastmod => cat.updated_at
+        add cat_path(cat, locale: locale), :priority => '1.0', :changefreq => 'always', :lastmod => cat.updated_at
       end
 
       add catteries_path, :changefreq => 'always'
       User.is_cattery(true).distinct(:id).joins(:cats).joins(:country).cattery_information_present.find_each do |cattery|
-        add cattery_path(cattery), :priority => '1.0', :changefreq => 'always', :lastmod => cattery.updated_at
+        add cattery_path(cattery, locale: locale), :priority => '1.0', :changefreq => 'always', :lastmod => cattery.updated_at
       end
 
       add breeds_path, :changefreq => 'weekly'
       Breed.find_each do |breed|
-        add breed_path(breed), :priority => '0.8', :changefreq => 'weekly', :lastmod => breed.updated_at
+        add breed_path(breed, locale: locale), :priority => '0.8', :changefreq => 'weekly', :lastmod => breed.updated_at
       end
-
     end
   end
-  
+
   add privacy_policy_path, :changefreq => 'monthly'
   add terms_of_service_path, :changefreq => 'monthly'
   add cookies_policy_path, :changefreq => 'monthly'
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 end
