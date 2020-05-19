2b365e72f1e7ecd066f3494d8c36d6c95b205a2c
SprouterB <wouter.bruynsteen@gmail.com>
2020-05-19 21:20:29 +0200

    Seed file breeds, coat patterns, colors and countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# # CAT COLORS AND COAT PATTERNS
-# coats = [{ id: 1, name: 'solid' }, { id: 2, name: 'bi_colour' }, { id: 3, name: 'tabby' }, { id: 4, name: 'tortoiseshell' }, { id: 5, name: 'tri_color_calico' }, { id: 6, name: 'colour_point' }]
-# colors = [{ id: 1, name: 'black' }, { id: 2, name: 'blue' }, { id: 3, name: 'brown' }, { id: 4, name: 'cinnamon' }, { id: 5, name: 'cream' }, { id: 6, name: 'fawn' }, { id: 7, name: 'grey' }, { id: 8, name: 'red_ginger' }, { id: 9, name: 'white' }]
+# CAT COLORS AND COAT PATTERNS
+coats = [{ name: "solid" }, { name: "bi-colour" }, { name: "tabby" }, { name: "tortoiseshell" }, { name: "tri color_calico" }, { name: "colour point" }, { name: "smoke" }]
+colors = [{ name: "black" }, { name: "blue" }, { name: "brown" }, { name: "cinnamon" }, { name: "cream" }, { name: "fawn" }, { name: "grey" }, { name: "red_ginger" }, { name: "white" }]
 
-# coats.each do |hash|
-#   coat = CoatPattern.find(hash[:id])
-#   coat.update(name: hash[:name])
-# end
+coats.each do |hash|
+  CoatPattern.create(name: hash[:name])
+end
 
-# colors.each do |hash|
-#   color = Color.find(hash[:id])
-#   color.update(name: hash[:name])
-# end
+colors.each do |hash|
+  Color.create(name: hash[:name])
+end
 
-# # BREEDS
-# breeds = { id: 1, name: 'American Shorthair', breed_code: 'AS' },{ id: 2, name: 'Bengal', breed_code: 'BG' },{ id: 3, name: 'Birman', breed_code: 'BI' },{ id: 4, name: 'Bombay', breed_code: 'BO' },{ id: 5, name: 'British Shorthair', breed_code: 'BS' },{ id: 6, name: 'Burmese', breed_code: 'BU' },{ id: 7, name: 'Chartreux', breed_code: 'CX' },{ id: 8, name: 'Himalayan', breed_code: 'HI' },{ id: 9, name: 'Maine Coon', breed_code: 'MC' },{ id: 10, name: 'Munchkin', breed_code: 'MK' },{ id: 11, name: 'Nebelung', breed_code: 'NB' },{ id: 12, name: 'Norwegian Forest Cat', breed_code: 'NF' },{ id: 13, name: 'Persian', breed_code: 'PS' },{ id: 14, name: 'Ragamuffin', breed_code: 'RM' },{ id: 15, name: 'Ragdoll', breed_code: 'RD' },{ id: 16, name: 'Russian Blue', breed_code: 'RB' },{ id: 17, name: 'Scottish Fold', breed_code: 'SF' },{ id: 18, name: 'Siamese', breed_code: 'SI' },{ id: 19, name: 'Siberian', breed_code: 'SB' },{ id: 20, name: 'Sphynx', breed_code: 'SX' }
+# BREEDS
+breeds = [{ name: "American Shorthair", breed_code: "AS" },{ name: "Bengal", breed_code: "BG" },{ name: "Birman", breed_code: "BI" },{ name: "Bombay", breed_code: "BO" },{ name: "British Shorthair", breed_code: "BS" },{ name: "Burmese", breed_code: "BU" },{ name: "Chartreux", breed_code: "CX" },{ name: "Himalayan", breed_code: "HI" },{ name: "Maine Coon", breed_code: "MC" },{ name: "Munchkin", breed_code: "MK" },{ name: "Nebelung", breed_code: "NB" },{ name: "Norwegian Forest Cat", breed_code: "NF" },{ name: "Persian", breed_code: "PS" },{ name: "Ragamuffin", breed_code: "RM" },{ name: "Ragdoll", breed_code: "RD" },{ name: "Russian Blue", breed_code: "RB" },{ name: "Scottish Fold", breed_code: "SF" },{ name: "Siamese", breed_code: "SI" },{ name: "Siberian", breed_code: "SB" },{ name: "Sphynx", breed_code: "SX" }]
 
-# breeds.each do |breed_hash|
-#   breed = Breed.find(breed_hash[:id])
-#   breed.update(breed_code: breed_hash[:breed_code])
-# end
+breeds.each do |breed_hash|
+  Breed.create(name: breed_hash[:name], breed_code: breed_hash[:breed_code])
+end
 
 # UPDATE BREEDS INFORMATION
 # breeds = [ { id: 10, filepath: '../Images/munchkin_placeholder.jpg', filename: 'munchkin_placeholder.jpg' } ]
@@ -36,30 +33,30 @@
 #   breed = Breed.find(breed_hash[:id])
 #   breed.picture.purge
 #   breed.picture.attach(io: File.open(breed_hash[:filepath]), filename: breed_hash[:filename], content_type: 'image/jpeg')
-  # breed.update( short_description: breed_hash[:short_description]
-                # personality: breed_hash[:personality],
-                # history: breed_hash[:history],
-                # playfulness: breed_hash[:playfulness],
-                # activity_level: breed_hash[:activity_level],
-                # friendliness_to_other_pets: breed_hash[:friendliness_to_other_pets],
-                # friendliness_to_children: breed_hash[:friendliness_to_children],
-                # grooming_requirements: breed_hash[:grooming_requirements],
-                # vocality: breed_hash[:vocality],
-                # need_for_attention: breed_hash[:need_for_attention],
-                # affection_toward_its_owners: breed_hash[:affection_toward_its_owners],
-                # docility: breed_hash[:docility],
-                # intelligence: breed_hash[:intelligence],
-                # independence: breed_hash[:independence],
-                # hardiness: breed_hash[:hardiness]
+# breed.update( short_description: breed_hash[:short_description]
+              # personality: breed_hash[:personality],
+              # history: breed_hash[:history],
+              # playfulness: breed_hash[:playfulness],
+              # activity_level: breed_hash[:activity_level],
+              # friendliness_to_other_pets: breed_hash[:friendliness_to_other_pets],
+              # friendliness_to_children: breed_hash[:friendliness_to_children],
+              # grooming_requirements: breed_hash[:grooming_requirements],
+              # vocality: breed_hash[:vocality],
+              # need_for_attention: breed_hash[:need_for_attention],
+              # affection_toward_its_owners: breed_hash[:affection_toward_its_owners],
+              # docility: breed_hash[:docility],
+              # intelligence: breed_hash[:intelligence],
+              # independence: breed_hash[:independence],
+              # hardiness: breed_hash[:hardiness]
 # )
 # end
 
 # # COUNTRIES
+countries = [ { country_code: "AD", name: "Andorra" }, { country_code: "AE", name: "United Arab Emirates" }, { country_code: "AF", name: "Afghanistan" }, { country_code: "AG", name: "Antigua and Barbuda" }, { country_code: "AI", name: "Anguilla" }, { country_code: "AL", name: "Albania" }, { country_code: "AM", name: "Armenia" }, { country_code: "AO", name: "Angola" }, { country_code: "AQ", name: "Antarctica" }, { country_code: "AR", name: "Argentina" }, { country_code: "AS", name: "American Samoa" }, { country_code: "AT", name: "Austria" }, { country_code: "AU", name: "Australia" }, { country_code: "AW", name: "Aruba" }, { country_code: "AX", name: "Åland Islands" }, { country_code: "AZ", name: "Azerbaijan" }, { country_code: "BA", name: "Bosnia and Herzegovina" }, { country_code: "BB", name: "Barbados" }, { country_code: "BD", name: "Bangladesh" }, { country_code: "BE", name: "Belgium" }, { country_code: "BF", name: "Burkina Faso" }, { country_code: "BG", name: "Bulgaria" }, { country_code: "BH", name: "Bahrain" }, { country_code: "BI", name: "Burundi" }, { country_code: "BJ", name: "Benin" }, { country_code: "BL", name: "Saint Barthélemy" }, { country_code: "BM", name: "Bermuda" }, { country_code: "BN", name: "Brunei Darussalam" }, { country_code: "BO", name: "Bolivia (Plurinational State of)" }, { country_code: "BQ", name: "Bonaire, Sint Eustatius and Saba" }, { country_code: "BR", name: "Brazil" }, { country_code: "BS", name: "Bahamas" }, { country_code: "BT", name: "Bhutan" }, { country_code: "BV", name: "Bouvet Island" }, { country_code: "BW", name: "Botswana" }, { country_code: "BY", name: "Belarus" }, { country_code: "BZ", name: "Belize" }, { country_code: "CA", name: "Canada" }, { country_code: "CC", name: "Cocos (Keeling) Islands" }, { country_code: "CD", name: "Congo, Democratic Republic of the" }, { country_code: "CF", name: "Central African Republic" }, { country_code: "CG", name: "Congo" }, { country_code: "CH", name: "Switzerland" }, { country_code: "CI", name: "Côte d\'Ivoire" }, { country_code: "CK", name: "Cook Islands" }, { country_code: "CL", name: "Chile" }, { country_code: "CM", name: "Cameroon" }, { country_code: "CN", name: "China" }, { country_code: "CO", name: "Colombia" }, { country_code: "CR", name: "Costa Rica" }, { country_code: "CU", name: "Cuba" }, { country_code: "CV", name: "Cabo Verde" }, { country_code: "CW", name: "Curaçao" }, { country_code: "CX", name: "Christmas Island" }, { country_code: "CY", name: "Cyprus" }, { country_code: "CZ", name: "Czechia" }, { country_code: "DE", name: "Germany" }, { country_code: "DJ", name: "Djibouti" }, { country_code: "DK", name: "Denmark" }, { country_code: "DM", name: "Dominica" }, { country_code: "DO", name: "Dominican Republic" }, { country_code: "DZ", name: "Algeria" }, { country_code: "EC", name: "Ecuador" }, { country_code: "EE", name: "Estonia" }, { country_code: "EG", name: "Egypt" }, { country_code: "EH", name: "Western Sahara" }, { country_code: "ER", name: "Eritrea" }, { country_code: "ES", name: "Spain" }, { country_code: "ET", name: "Ethiopia" }, { country_code: "FI", name: "Finland" }, { country_code: "FJ", name: "Fiji" }, { country_code: "FK", name: "Falkland Islands (Malvinas)" }, { country_code: "FM", name: "Micronesia (Federated States of)" }, { country_code: "FO", name: "Faroe Islands" }, { country_code: "FR", name: "France" }, { country_code: "GA", name: "Gabon" }, { country_code: "GB", name: "United Kingdom of Great Britain and Northern Ireland" }, { country_code: "GD", name: "Grenada" }, { country_code: "GE", name: "Georgia" }, { country_code: "GF", name: "French Guiana" }, { country_code: "GG", name: "Guernsey" }, { country_code: "GH", name: "Ghana" }, { country_code: "GI", name: "Gibraltar" }, { country_code: "GL", name: "Greenland" }, { country_code: "GM", name: "Gambia" }, { country_code: "GN", name: "Guinea" }, { country_code: "GP", name: "Guadeloupe" }, { country_code: "GQ", name: "Equatorial Guinea" }, { country_code: "GR", name: "Greece" }, { country_code: "GS", name: "South Georgia and the South Sandwich Islands" }, { country_code: "GT", name: "Guatemala" }, { country_code: "GU", name: "Guam" }, { country_code: "GW", name: "Guinea-Bissau" }, { country_code: "GY", name: "Guyana" }, { country_code: "HK", name: "Hong Kong" }, { country_code: "HM", name: "Heard Island and McDonald Islands" }, { country_code: "HN", name: "Honduras" }, { country_code: "HR", name: "Croatia" }, { country_code: "HT", name: "Haiti" }, { country_code: "HU", name: "Hungary" }, { country_code: "ID", name: "Indonesia" }, { country_code: "IE", name: "Ireland" }, { country_code: "IL", name: "Israel" }, { country_code: "IM", name: "Isle of Man" }, { country_code: "IN", name: "India" }, { country_code: "IO", name: "British Indian Ocean Territory" }, { country_code: "IQ", name: "Iraq" }, { country_code: "IR", name: "Iran (Islamic Republic of)" }, { country_code: "IS", name: "Iceland" }, { country_code: "IT", name: "Italy" }, { country_code: "JE", name: "Jersey" }, { country_code: "JM", name: "Jamaica" }, { country_code: "JO", name: "Jordan" }, { country_code: "JP", name: "Japan" }, { country_code: "KE", name: "Kenya" }, { country_code: "KG", name: "Kyrgyzstan" }, { country_code: "KH", name: "Cambodia" }, { country_code: "KI", name: "Kiribati" }, { country_code: "KM", name: "Comoros" }, { country_code: "KN", name: "Saint Kitts and Nevis" }, { country_code: "KP", name: "Korea (Democratic People\'s Republic of)" }, { country_code: "KR", name: "Korea, Republic of" }, { country_code: "KW", name: "Kuwait" }, { country_code: "KY", name: "Cayman Islands" }, { country_code: "KZ", name: "Kazakhstan" }, { country_code: "LA", name: "Lao People\'s Democratic Republic" }, { country_code: "LB", name: "Lebanon" }, { country_code: "LC", name: "Saint Lucia" }, { country_code: "LI", name: "Liechtenstein" }, { country_code: "LK", name: "Sri Lanka" }, { country_code: "LR", name: "Liberia" }, { country_code: "LS", name: "Lesotho" }, { country_code: "LT", name: "Lithuania" }, { country_code: "LU", name: "Luxembourg" }, { country_code: "LV", name: "Latvia" }, { country_code: "LY", name: "Libya" }, { country_code: "MA", name: "Morocco" }, { country_code: "MC", name: "Monaco" }, { country_code: "MD", name: "Moldova, Republic of" }, { country_code: "ME", name: "Montenegro" }, { country_code: "MF", name: "Saint Martin (French part)" }, { country_code: "MG", name: "Madagascar" }, { country_code: "MH", name: "Marshall Islands" }, { country_code: "MK", name: "North Macedonia" }, { country_code: "ML", name: "Mali" }, { country_code: "MM", name: "Myanmar" }, { country_code: "MN", name: "Mongolia" }, { country_code: "MO", name: "Macao" }, { country_code: "MP", name: "Northern Mariana Islands" }, { country_code: "MQ", name: "Martinique" }, { country_code: "MR", name: "Mauritania" }, { country_code: "MS", name: "Montserrat" }, { country_code: "MT", name: "Malta" }, { country_code: "MU", name: "Mauritius" }, { country_code: "MV", name: "Maldives" }, { country_code: "MW", name: "Malawi" }, { country_code: "MX", name: "Mexico" }, { country_code: "MY", name: "Malaysia" }, { country_code: "MZ", name: "Mozambique" }, { country_code: "NA", name: "Namibia" }, { country_code: "NC", name: "New Caledonia" }, { country_code: "NE", name: "Niger" }, { country_code: "NF", name: "Norfolk Island" }, { country_code: "NG", name: "Nigeria" }, { country_code: "NI", name: "Nicaragua" }, { country_code: "NL", name: "Netherlands" }, { country_code: "NO", name: "Norway" }, { country_code: "NP", name: "Nepal" }, { country_code: "NR", name: "Nauru" }, { country_code: "NU", name: "Niue" }, { country_code: "NZ", name: "New Zealand" }, { country_code: "OM", name: "Oman" }, { country_code: "PA", name: "Panama" }, { country_code: "PE", name: "Peru" }, { country_code: "PF", name: "French Polynesia" }, { country_code: "PG", name: "Papua New Guinea" }, { country_code: "PH", name: "Philippines" }, { country_code: "PK", name: "Pakistan" }, { country_code: "PL", name: "Poland" }, { country_code: "PM", name: "Saint Pierre and Miquelon" }, { country_code: "PN", name: "Pitcairn" }, { country_code: "PR", name: "Puerto Rico" }, { country_code: "PS", name: "Palestine, State of" }, { country_code: "PT", name: "Portugal" }, { country_code: "PW", name: "Palau" }, { country_code: "PY", name: "Paraguay" }, { country_code: "QA", name: "Qatar" }, { country_code: "RE", name: "Réunion" }, { country_code: "RO", name: "Romania" }, { country_code: "RS", name: "Serbia" }, { country_code: "RU", name: "Russian Federation" }, { country_code: "RW", name: "Rwanda" }, { country_code: "SA", name: "Saudi Arabia" }, { country_code: "SB", name: "Solomon Islands" }, { country_code: "SC", name: "Seychelles" }, { country_code: "SD", name: "Sudan" }, { country_code: "SE", name: "Sweden" }, { country_code: "SG", name: "Singapore" }, { country_code: "SH", name: "Saint Helena, Ascension and Tristan da Cunha" }, { country_code: "SI", name: "Slovenia" }, { country_code: "SJ", name: "Svalbard and Jan Mayen" }, { country_code: "SK", name: "Slovakia" }, { country_code: "SL", name: "Sierra Leone" }, { country_code: "SM", name: "San Marino" }, { country_code: "SN", name: "Senegal" }, { country_code: "SO", name: "Somalia" }, { country_code: "SR", name: "Suriname" }, { country_code: "SS", name: "South Sudan" }, { country_code: "ST", name: "Sao Tome and Principe" }, { country_code: "SV", name: "El Salvador" }, { country_code: "SX", name: "Sint Maarten (Dutch part)" }, { country_code: "SY", name: "Syrian Arab Republic" }, { country_code: "SZ", name: "Eswatini" }, { country_code: "TC", name: "Turks and Caicos Islands" }, { country_code: "TD", name: "Chad" }, { country_code: "TF", name: "French Southern Territories" }, { country_code: "TG", name: "Togo" }, { country_code: "TH", name: "Thailand" }, { country_code: "TJ", name: "Tajikistan" }, { country_code: "TK", name: "Tokelau" }, { country_code: "TL", name: "Timor-Leste" },
+ { country_code: "TM", name: "Turkmenistan" }, { country_code: "TN", name: "Tunisia" }, { country_code: "TO", name: "Tonga" }, { country_code: "TR", name: "Turkey" }, { country_code: "TT", name: "Trinidad and Tobago" }, { country_code: "TV", name: "Tuvalu" }, { country_code: "TW", name: "Taiwan, Province of China" }, { country_code: "TZ", name: "Tanzania, United Republic of" }, { country_code: "UA", name: "Ukraine" }, { country_code: "UG", name: "Uganda" }, { country_code: "UM", name: "United States Minor Outlying Islands" }, { country_code: "US", name: "United States of America" }, { country_code: "UY", name: "Uruguay" }, { country_code: "UZ", name: "Uzbekistan" }, { country_code: "VA", name: "Holy See" }, { country_code: "VC", name: "Saint Vincent and the Grenadines" }, { country_code: "VE", name: "Venezuela (Bolivarian Republic of)" }, { country_code: "VG", name: "Virgin Islands (British)" }, { country_code: "VI", name: "Virgin Islands (U.S.)" }, { country_code: "VN", name: "Viet Nam" }, { country_code: "VU", name: "Vanuatu" }, { country_code: "WF", name: "Wallis and Futuna" }, { country_code: "WS", name: "Samoa" }, { country_code: "YE", name: "Yemen" }, { country_code: "YT", name: "Mayotte" }, { country_code: "ZA", name: "South Africa" }, { country_code: "ZM", name: "Zambia" }, { country_code: "ZW", name: "Zimbabwe" } ]
 
-# countries.each do |country_hash|
-#   country = Country.find(country_hash[:id])
-#   country.update(name: country_hash[:name], country_code: country_hash[:country_code])
-
-# end
+ countries.each do |country_hash|
+  country.create(name: country_hash[:name], country_code: country_hash[:country_code])
+end
 
 AdminUser.create!(email: 'support@catlinker.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
